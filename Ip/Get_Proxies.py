be962e28d74af9fd946c08f36c415cc9cae83ae0
lucas.wang <lucas.wang@ariix.com>
2018-02-08 10:21:48 +0800

    Get proxies list

--- Ip/Get_Proxies.py
+++ Ip/Get_Proxies.py
@@ -11,36 +11,39 @@ from bs4 import BeautifulSoup
 import requests
 import random
 
-def get_ip_list(url, headers):
-    web_data = requests.get(url, headers=headers)
-    # print(web_data)
-    soup = BeautifulSoup(web_data.text, 'lxml')
-    # print(soup)
-    ips = soup.find_all('tr')
-    # print(ips)
-    ip_list = []
-    for i in range(1, len(ips)):
-        ip_info = ips[i]
-        # print(ip_info)
-        tds = ip_info.find_all('td')
-        # print(tds)
-        ip_list.append(tds[1].text + ':' + tds[2].text)
-    return ip_list
+class Get_proxies(object):
+    def get_ip_list(self, url, headers):
+        web_data = requests.get(url, headers=headers)
+        # print(web_data)
+        soup = BeautifulSoup(web_data.text, 'lxml')
+        # print(soup)
+        ips = soup.find_all('tr')
+        # print(ips)
+        ip_list = []
+        for i in range(1, len(ips)):
+            ip_info = ips[i]
+            # print(ip_info)
+            tds = ip_info.find_all('td')
+            # print(tds)
 
-def get_random_ip(ip_list):
-    proxy_list = []
-    for ip in ip_list:
-        proxy_list.append('http://' + ip)
-    print(proxy_list)
-    proxy_ip = random.choice(proxy_list)
-    proxies = {'http': proxy_ip}
-    return proxies
+            ip_list.append(tds[1].text + ':' + tds[2].text)
+        return ip_list
+
+    def get_random_ip(self, ip_list):
+        proxy_list = []
+        for ip in ip_list:
+            proxy_list.append('http://' + ip)
+        # print(proxy_list)
+        proxy_ip = random.choice(proxy_list)
+        proxies = {'http': proxy_ip, }
+        return proxy_ip
 
 if __name__ == '__main__':
     url = 'http://www.xicidaili.com/nn/'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36'
     }
-    ip_list = get_ip_list(url, headers=headers)
-    proxies = get_random_ip(ip_list)
+    getProxies = Get_proxies()
+    ip_list = getProxies.get_ip_list(url, headers=headers)
+    proxies = getProxies.get_random_ip(ip_list)
     print(proxies)
