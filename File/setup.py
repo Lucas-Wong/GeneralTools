e3ce37c48f124ced71e72c50ef8ca6d551bbb2dd
lucas.wang <lucas.wang@ariix.com>
2018-05-09 16:01:06 +0800

    setting setup file

--- File/setup.py
+++ File/setup.py
@@ -3,7 +3,18 @@
 """
 @author = lucas.wang 
 @create_time = 2018-03-22 
+1. python setup.py sdist    # generate setup box
+2. python setup.py install  # install to local
 """
 
-if __name__ == '__main__':
-    pass
+from distutils.core import setup
+
+setup(
+    name= '',
+    version='',
+    py_modules = '',
+    author= 'Lucas wong',
+    author_email='Lucaswong@163.com',
+    url='',
+    description='',
+)
